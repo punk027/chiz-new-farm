f991b9516be6ed984d59dbbad8f211226c0e73cf
jasonGhostDev <hyotiger777@gmail.com>
2021-09-17 23:06:55 +1000

    update pool info

--- src/contracts/lib/constants.js
+++ src/contracts/lib/constants.js
@@ -2,12 +2,12 @@
 export const contractAddresses = {
   erc20: {
     42: "0x175D9b7BEf31cC753D1d94Ee70d90F23aBF94070",
-    1: "0x0",
+    1: "0x5c761c1a21637362374204000e383204d347064c",
     4: "0x2f997B58a2a21f179dd76De40aA2277C81948084",
   },
   farm: {
     42: "0x91B35B2f90f283A093e14B2d8578Afcd67C18876",
-    1: "0x0",
+    1: "0x5A2d9d775053aEde7832CD2Ac9De3abc13e53dF2",
     4: "0x439607d5506E009b98fC9E37dD29E17b096c472f",
   },
   weth: {
@@ -23,37 +23,16 @@ export const supportedPools = [
     pid: 0,
     lpAddresses: {
       42: "0xf9f89F9dF418664cFAF5f8669d846269e49Ca6f1",
-      1: "0x0",
+      1: "0x6b8BC6cEbaA266429F387125D433E46F59CDb499",
       4: "0x2fb106f9751d643452f4c8a657ad408341824108",
     },
     farmAddresses: {
       4: "0x439607d5506E009b98fC9E37dD29E17b096c472f",
+      1: "0x5A2d9d775053aEde7832CD2Ac9De3abc13e53dF2"
     },
     tokenAddresses: {
       42: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
-      1: "0x0",
-      4: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
-    },
-    name: "CHIZ/ETH",
-    symbol: "CHIZ/ETH",
-    tokenSymbol: "CHIZ",
-  
-    poolTitle: "CHIZ/ETH Pool",
-   // pool: "50%",
-  },
-  {
-    pid: 1,
-    lpAddresses: {
-      42: "0xf9f89F9dF418664cFAF5f8669d846269e49Ca6f1",
-      1: "0x0",
-      4: "0x2fb106f9751d643452f4c8a657ad408341824108",
-    },
-    farmAddresses: {
-      4: "0x439607d5506E009b98fC9E37dD29E17b096c472f",
-    },
-    tokenAddresses: {
-      42: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
-      1: "0x0",
+      1: "0x5c761c1a21637362374204000e383204d347064C",
       4: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
     },
     name: "CHIZ/ETH",
@@ -62,6 +41,53 @@ export const supportedPools = [
   
     poolTitle: "EXTRA CHIZ/ETH Pool",
     pool: "Must have at least one Rat and one Cheddaz companion in your wallet.",
+   
+   // pool: "50%",
+  },
+  {
+    pid: 1,
+    lpAddresses: {
+      42: "0xf9f89F9dF418664cFAF5f8669d846269e49Ca6f1",
+      1: "0x6b8BC6cEbaA266429F387125D433E46F59CDb499",
+      4: "0x2fb106f9751d643452f4c8a657ad408341824108",
+    },
+    farmAddresses: {
+      4: "0x439607d5506E009b98fC9E37dD29E17b096c472f",
+      1: "0x5A2d9d775053aEde7832CD2Ac9De3abc13e53dF2"
+    },
+    tokenAddresses: {
+      42: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
+      1: "0x5c761c1a21637362374204000e383204d347064C",
+      4: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
+    },
+    name: "CHIZ/ETH",
+    symbol: "CHIZ/ETH",
+    tokenSymbol: "CHIZ",
+  
+    poolTitle: "CHIZ/ETH Pool",
+  },
+  {
+    pid: 2,
+    lpAddresses: {
+      42: "0xf9f89F9dF418664cFAF5f8669d846269e49Ca6f1",
+      1: "0x02346c6d7DCDCbfa511Eb72BF61f7545f7bD2527",
+      4: "0x2fb106f9751d643452f4c8a657ad408341824108",
+    },
+    farmAddresses: {
+      4: "0x439607d5506E009b98fC9E37dD29E17b096c472f",
+      1: "0x5A2d9d775053aEde7832CD2Ac9De3abc13e53dF2"
+    },
+    tokenAddresses: {
+      42: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
+      1: "0x5c761c1a21637362374204000e383204d347064C",
+      4: "0xded7ceb95a1f3c20f07545dc50a8d2c093bbdf22",
+    },
+    name: "SRSC20/ETH",
+    symbol: "SRSC20/ETH",
+    tokenSymbol: "CHIZ",
+  
+    poolTitle: "SRSC20/ETH Pool",
+   
   },
   
 ];
